45742aa8a7f7e546224fd2a3b94ce2a35fe458c7
qiafengchen <67124360+qiafengchen@users.noreply.github.com>
2023-07-08 20:06:19 -0400

    updated API key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://kohls.p.rapidapi.com',
   headers: {
-    'X-RapidAPI-Key': '731e179662mshf81f7e3ef00701ap1af09ajsnf96aca230cd8',
+    'X-RapidAPI-Key': 'f612429eccmsh11768d3a395f8fep1f9bbajsn49a589b63780',
     'X-RapidAPI-Host': 'kohls.p.rapidapi.com',
   },
 });
